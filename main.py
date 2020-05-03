b7177ea6c6de0cc25d39f37dc91dcc4d7d0ebe48
Nicholas Ornstein <44384650+nbso13@users.noreply.github.com>
2020-05-03 03:23:38 -0500

    Saving previous conversation, only speaks when spoken to.
    Ok so now what it does:
    1. Logs into both chat rooms
    2. When a user messages, checks list of messages from the other user.
       IF empty, choose randomly from a list of replies
       ELSE reply with most recent statement and takes it off the list.
    This way, the bot will only respond when spoken to.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from twitchobserver import Observer
 import time
 import threading
+import random
 
 # change these to be, well, you
 #using 'bot_tester_bot' twitch account, password 'bot_tester_bo'.
@@ -13,11 +14,29 @@ if (AUTHKEY=='UNSET'):
     raise NotImplementedError("this isn't going to work, you need to set your username and authkey")
 
 # your code goes here!
-def message(text, user):
-    return "%s to you too, %s" % (text, user)
+def message(user):
+    # figure out which channel we're dealing with
+    if user == CHANNEL_1:
+        ind = 1 #refering to chan2
+    else:
+        ind = 0 #ref chan1
+    if not(chan_replies[ind]): #if empty list
+        #choose a random default reply
+        text = defaultReplies[random.randint(0, len(defaultReplies)-1)]
+    else:
+        #otherwise take the last of the replies from the other channel
+        text = chan_replies[ind].pop()
+    return text
 
 # we keep a list of message IDs we've seen before just in case we see them twice
 seenBefore = set()
+
+
+# save replies from each person: first list is chan 1, second is chan 2
+chan_replies = list([list(), list()])
+
+# if you get texted and the other person hasn't said anything, use this
+defaultReplies = list(["Nah fuck YOU.", "Ikr?!", "The odds point to ~maybe~"])
 
 print("connecting")
 observer = Observer(USER, AUTHKEY)
@@ -50,12 +69,14 @@ with observer:
                 print(event)
                 if event.type == 'TWITCHCHATMESSAGE':
                     if CHANNEL_1 == event.channel:
-                        new_chan = CHANNEL_2
+                        chan_ind = 0 #came from first chan
                     else:
-                        new_chan = CHANNEL_1
-                    reply = message(event.message, new_chan)
+                        chan_ind = 1 #came from first chan
+                    # add to appropriate list
+                    chan_replies[chan_ind].append(event.message)
+                    reply = message(event.channel)
                     if (reply):
-                        observer.send_message(reply, new_chan)
+                        observer.send_message(reply, event.channel)
 
             # never post too fast, even in response to messages that arrive. Twitch will block us.
             time.sleep(0.1)
